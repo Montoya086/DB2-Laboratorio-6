08d2bebfbb38d2715b76d3d3924569b4addd5490
Andrés Montoya <montoyaw1@gmail.com>
2024-04-06 21:23:40 -0600

    updated to merge
    updated to merge to avoid dupes

--- src/creation/create_node.py
+++ src/creation/create_node.py
@@ -5,6 +5,6 @@ def create_node(graph: Graph, labels, properties = {})-> str:
     # Unir todas las etiquetas en una cadena separada por dos puntos.
     label_str = ":".join(labels)
     properties["uuid"] = str(uuid4())
-    query = f"CREATE (n:{label_str} $properties)"
+    query = f"MERGE (n:{label_str} $properties)"
     graph.run(query, properties=properties)
     return properties["uuid"]
--- src/creation/create_relation.py
+++ src/creation/create_relation.py
@@ -3,6 +3,6 @@ from uuid import uuid4
 
 def create_relation(graph: Graph, node1UUID: str, node2UUID: str, relation: str, properties = {}) -> str:
     properties["uuid"] = str(uuid4())
-    query = f"MATCH (a) WHERE a.uuid = '{node1UUID}' MATCH (b) WHERE b.uuid = '{node2UUID}' CREATE (a)-[r:{relation} $properties]->(b)"
+    query = f"MATCH (a) WHERE a.uuid = '{node1UUID}' MATCH (b) WHERE b.uuid = '{node2UUID}' MERGE (a)-[r:{relation} $properties]->(b)"
     graph.run(query, properties=properties)
     return properties["uuid"]
