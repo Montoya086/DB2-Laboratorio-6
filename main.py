9ffba021ca9043487ba86568e625dc8374aad554
Andrés Montoya <montoyaw1@gmail.com>
2024-04-06 21:55:46 -0600

    Rating Graph creation

--- main.py
+++ main.py
@@ -1,33 +1,51 @@
 from src.creation.create_node import create_node as NodeGraph
 from src.creation.create_relation import create_relation as RelationGraph
 from py2neo import Graph
+from uuid import uuid4
+from faker import Faker
+from random import randint, choice
+
+fake = Faker()
 
 graph = Graph("neo4j+s://030df7e1.databases.neo4j.io", auth=("neo4j", "b4VfuDIDUCu3Cbj4SpAxCjZmJtrdEkhy-nCCSMREPbg"))
 
-def main():
-    labels = ["Person", "Customer"]
-    properties = {
-        "name": "John Doe",
-        "email": "john@doe.com",
-    }
+def main_1():
+    users = []
+    movies = []
+    for _ in range(5):
+        user_name = fake.name()
+        user_id = str(uuid4())
+        labels = ["USER"]
+        properties = {
+            "name": user_name,
+            "userId": user_id,
+        }
+        user_node_id = NodeGraph(graph, labels, properties)
+        users.append(user_node_id)
 
-    n1=NodeGraph(graph, labels, properties)
+    for _ in range(10):
+        movie_title = fake.company()
+        movie_id = randint(1000, 9999)
+        labels = ["MOVIE"]
+        properties = {
+            "title": movie_title,
+            "movieId": movie_id,
+            "year": int(fake.year()),
+            "plot": fake.text(),
+        }
+        movie_node_id = NodeGraph(graph, labels, properties)
+        movies.append(movie_node_id)
 
-    labels = ["Person", "Customer"]
-    properties = {
-        "name": "Jane Doe",
-        "email": "jane@doe.com",
-    }
-    n2 = NodeGraph(graph, labels, properties)
-
-    relation = "KNOWS"
-    properties = {
-        "since": "2020-01-01",
-    }
-    r1 = RelationGraph(graph, n1, n2, relation, properties)
-
-    print(f"Relation UUID: {r1}")
+    for user_node_id in users:
+        for _ in range(2):
+            movie_node_id = choice(movies)
+            rating = randint(0, 5)
+            properties = {
+                "rating": rating,
+                "timestamp": fake.unix_time(),
+            }
+            RelationGraph(graph, user_node_id, movie_node_id, "RATED", properties)
 
 
 if __name__ == "__main__":
-    main()
+    main_1()
--- src/creation/create_node.py
+++ src/creation/create_node.py
@@ -5,6 +5,6 @@ def create_node(graph: Graph, labels: list[str], properties = {})-> str:
     # Unir todas las etiquetas en una cadena separada por dos puntos.
     label_str = ":".join(labels)
     properties["uuid"] = str(uuid4())
-    query = f"MERGE (n:{label_str} $properties)"
+    query = f"CREATE (n:{label_str} $properties)"
     graph.run(query, properties=properties)
     return properties["uuid"]
