b92bb35f0450446cc2e97b506550a7b660d13cee
Deloz09 <84214640+TheDeloz-v2@users.noreply.github.com>
2024-04-09 12:49:59 -0600

    Query User y Query Movie

--- src/query/user_movie_query.py
+++ src/query/user_movie_query.py
@@ -3,3 +3,11 @@ from py2neo import Graph
 def user_movie_query(graph: Graph, user1UUID: str, movie1UUID: str):
     query = f"MATCH (a:USER {{uuid: '{user1UUID}'}})-[r:RATED]->(b:MOVIE {{uuid: '{movie1UUID}'}}) RETURN a, r, b"
     return graph.run(query).data()
+
+def user_query(graph: Graph, user1UUID: str):
+    query = f"MATCH (a:USER {{uuid: '{user1UUID}'}}) RETURN a"
+    return graph.run(query).data()
+
+def user_query(graph: Graph, movie1UUID: str):
+    query = f"MATCH (a:MOVIE {{uuid: '{movie1UUID}'}}) RETURN a"
+    return graph.run(query).data()
