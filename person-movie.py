aec1630cc57b2c77c7d286ef596c3a3e2c32b370
Deloz09 <84214640+TheDeloz-v2@users.noreply.github.com>
2024-04-09 15:50:54 -0600

    Inciso 4

--- person-movie.py
+++ person-movie.py
@@ -8,18 +8,14 @@ from datetime import datetime
 
 fake = Faker()
 
-# graph = Graph("neo4j+s://030df7e1.databases.neo4j.io", auth=("neo4j", "b4VfuDIDUCu3Cbj4SpAxCjZmJtrdEkhy-nCCSMREPbg"))
-
-class person:
-    def __init__(self, name, tmbdId, born, died, bornIn, url, inmdbId, bio, poster):
-        self.name = name
-
+graph = Graph("neo4j+s://030df7e1.databases.neo4j.io", auth=("neo4j", "b4VfuDIDUCu3Cbj4SpAxCjZmJtrdEkhy-nCCSMREPbg"))
 
 def main():
     persons = []
     users = []
     movies = []
-
+    genres = []
+    
     for _ in range(5):
         user_name = fake.name()
         user_id = str(uuid4())
@@ -28,9 +24,33 @@ def main():
             "name": user_name,
             "userId": user_id,
         }
-        # user_node_id = NodeGraph(graph, labels, properties)
-        # users.append(user_node_id)
-        users.append(properties)
+        user_node_id = NodeGraph(graph, labels, properties)
+        users.append(user_node_id)
+
+    for _ in range(10):
+        movie_title = fake.company()
+        movie_id = randint(1000, 9999)
+        labels = ["MOVIE"]
+        properties = {
+            "title": movie_title,
+            "tmdbId": randint(1000, 9999),
+            "released": fake.date(),
+            "imdbRating": randint(0, 10),
+            "movieId": movie_id,
+            "year": int(fake.year()),
+            'imdbId': str(uuid4()), 
+            "runtime": randint(10, 420),
+            "countries": fake.location_on_land()[3],
+            "imdbVotes": randint(10, 9999),
+            "url": fake.uri(),
+            "revenue": randint(10, 99999999),
+            "plot": fake.text(),
+            "poster": fake.image_url(),
+            "budget": randint(10, 99999999),
+            "languages": fake.language_code(),
+        }
+        movie_node_id = NodeGraph(graph, labels, properties)
+        movies.append(movie_node_id)
         
     for _ in range(5):    
         labels = ["PERSON"]
@@ -41,10 +61,10 @@ def main():
             'born': born, 
             'died': fake.date_between(start_date=born), 
             'bornIn': fake.location_on_land()[2], 
-            'url':fake.uri(), 
-            'inmdbId': str(uuid4()), 
-            'bio':fake.paragraph(),
-            'poster':fake.image_url()
+            'url': fake.uri(), 
+            'imdbId': str(uuid4()), 
+            'bio': fake.paragraph(),
+            'poster': fake.image_url()
         }
         
         randi = randint(0,2)
@@ -53,63 +73,67 @@ def main():
         elif randi == 1:
             labels.append("DIRECTOR")
         elif randi == 2:
-            labels.append("DIRECTOR")
-            labels.append("ACTOR")
-        # person_node_id = NodeGraph(graph, labels, properties)
-        # person.append(user_node_id)
-        persons.append({"labels":labels,"properties":properties})
+            labels.append("ACTOR DIRECTOR")
+        
+        person_node_id = NodeGraph(graph, labels, properties)
+        persons.append(person_node_id)
 
-    for _ in range(10):
-        labels = ["MOVIE"]
-        release_date = datetime.strptime(fake.date(),"%Y-%m-%d")
-        countries = []
-        for _ in range(5):
-            countries.append(fake.location_on_land()[3])
+    listGenres = ['Action', 'Adventure', 'Animation', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'History', 'Horror', 'Music', 'Mystery', 'Romance', 'Science Fiction', 'TV Movie', 'Thriller', 'War', 'Western']
+    for genre in listGenres:
+        labels = ["GENRE"]
         properties = {
-            "title": fake.bs(),
-            "tmdbId": randint(1000, 9999),
-            "released": release_date,
-            "imdbRating":randint(0, 10),
-            "movieId":randint(1000, 9999),
-            "year" : release_date,
-            "runtime": randint(10, 420),
-            "countries": countries,
-            "imdbVotes": randint(10, 9999),
-            "url":fake.uri(),
-            "revenue":randint(10, 99999999),
-            "plot": fake.paragraph(),
-            "poster":fake.image_url(),
-            "budget":randint(10, 99999999),
-            "languages":countries
-            
+            'name': genre
         }
-        # movie_node_id = NodeGraph(graph, labels, properties)
-        movies.append(properties)
-
-    relations = []
+        genre_node_id = NodeGraph(graph, labels, properties)
+        genres.append(genre_node_id)
+        
+    # Relation between movie and genre "IN_GENRE"
+    for movie_node_id in movies:
+        for _ in range(2):
+            genre_node_id = choice(genres)
+            RelationGraph(graph, movie_node_id, genre_node_id, "IN_GENRE", {})
+            
+    # Relation between user and Movie "RATED"
     for user_node_id in users:
         for _ in range(2):
-            movie_node_id = choice(movies)['title']
+            movie_node_id = choice(movies)
+            rating = randint(0, 5)
             properties = {
-                "rating": randint(0, 5),
+                "rating": rating,
                 "timestamp": fake.unix_time(),
             }
-            # print(user_node_id, movie_node_id, properties)
-    #         # RelationGraph(graph, user_node_id, movie_node_id, "RATED", properties)
-    
-    for person in persons:
-        for _ in range(2):
-            movie_node_id = choice(movies)['title']
-            properties = {}
-            if 'ACTOR' in person['labels']:
-                properties["ACTED_IN"] = fake.name()
-            if 'DIRECTOR' in person['labels']:
-                properties["DIRECTED"] = fake.name()
-            # print(person["properties"]["name"], movie_node_id, properties)
-    #         # RelationGraph(graph, user_node_id, movie_node_id, "RATED", properties)
-    # print(movies)
-    # print(persons)
-    # print(relations)
+            RelationGraph(graph, user_node_id, movie_node_id, "RATED", properties)
+            
+    # Relation between PERSON DIRECTOR and Movie "DIRECTED"
+    for person_node_id in persons:
+        if "DIRECTOR" in person_node_id:
+            for _ in range(2):
+                movie_node_id = choice(movies)
+                properties = {
+                    "role": "director"
+                }
+                RelationGraph(graph, person_node_id, movie_node_id, "DIRECTED", properties)
+            
+    # Relation between PERSON ACTOR and Movie "ACTED_IN"
+    for person_node_id in persons:
+        if "ACTOR" in person_node_id:
+            for _ in range(2):
+                movie_node_id = choice(movies)
+                properties = {
+                    "role": "actor"
+                }
+                RelationGraph(graph, person_node_id, movie_node_id, "ACTED_IN", properties)
+                
+    # Relation between PERSON ACTOR DIRECTOR and Movie "ACTED_IN"
+    for person_node_id in persons:
+        if "ACTOR DIRECTOR" in person_node_id:
+            for _ in range(2):
+                movie_node_id = choice(movies)
+                properties = {
+                    "role": "actor"
+                }
+                RelationGraph(graph, person_node_id, movie_node_id, "ACTED_IN", properties)
+                RelationGraph(graph, person_node_id, movie_node_id, "DIRECTED", properties)
 
 if __name__ == "__main__":
     main()
